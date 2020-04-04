d433db2eb7ce1083c31a416925dc22a5e458b6c6
Igor <sauron@MacBook-Pro-Igor.local>
2020-04-04 12:26:23 +0300

    g

--- draw.c
+++ draw.c
@@ -25,22 +25,28 @@ int		draw_image(t_mlx *data)
 	cl_kernel			kernel;
 
 	ret = clGetPlatformIDs(1, &platform_id, NULL);
+	printf("platfrom ret = %d\n", ret);
 	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, NULL);
-	cl_uint tmp;
-	clGetDeviceInfo(device_id, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(tmp), &tmp, NULL);
-	printf("max compute_units = %d\n", tmp);
-	clGetDeviceInfo(device_id, CL_DEVICE_IMAGE_SUPPORT, sizeof(tmp), &tmp, NULL);
-	printf("image support = %s\n", tmp ? "true" : "false");;
-	clGetDeviceInfo(device_id, CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS, sizeof(tmp), &tmp, NULL);
-	printf("max dimensions = %d\n", tmp);
-	clGetDeviceInfo(device_id, CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(tmp), &tmp, NULL);
-	printf("max work_group size = %d\n", tmp);
-	clGetDeviceInfo(device_id, CL_DEVICE_TYPE_GPU, sizeof(tmp), &tmp, NULL);
-	printf("max INT vector width = %d\n", tmp);
+	printf("device ret = %d\n", ret);
+//	cl_uint tmp;
+//	clGetDeviceInfo(device_id, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(tmp), &tmp, NULL);
+//	printf("max compute_units = %d\n", tmp);
+//	clGetDeviceInfo(device_id, CL_DEVICE_IMAGE_SUPPORT, sizeof(tmp), &tmp, NULL);
+//	printf("image support = %s\n", tmp ? "true" : "false");;
+//	clGetDeviceInfo(device_id, CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS, sizeof(tmp), &tmp, NULL);
+//	printf("max dimensions = %d\n", tmp);
+//	clGetDeviceInfo(device_id, CL_DEVICE_MAX_WORK_GROUP_SIZE, sizeof(tmp), &tmp, NULL);
+//	printf("max work_group size = %d\n", tmp);
+//	size_t max_par;
+//	clGetDeviceInfo(device_id, CL_DEVICE_MAX_PARAMETER_SIZE, sizeof(size_t), &max_par, NULL);
+//	printf("max kernel parameter size = %zu\n", max_par);
 
 
 	context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
+	printf("context ret = %d\n", ret);
 	queue = clCreateCommandQueue(context, device_id, 0, &ret);
+	printf("queue ret = %d\n", ret);
+
 
 	/*
 	 * считать код ядра
@@ -66,7 +72,6 @@ int		draw_image(t_mlx *data)
 		count++;
 		free(line);
 	}
-	printf("str count = %zu\n", count);
 	close(fd);
 	/* выделяем память под массив count-строк */
 	source = (char **)malloc(sizeof(char *) * count);
@@ -82,47 +87,62 @@ int		draw_image(t_mlx *data)
 
 	/* создать бинарник из кода программы */
 	program = clCreateProgramWithSource(context, count, (const char **)source, NULL, &ret);
+	printf("program creation ret = %d\n", ret);
+
 
 	/* скомпилировать программу */
 	ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+	printf("program build ret = %d\n", ret);
+	if (ret != 0) {
+		size_t log_size;
+		clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+
+		char *log = (char *) malloc(log_size);
+
+		clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
+
+		printf("%s\n", log);
+	}
 
 	/* создать кернел, передваемое имя - название kernela в файле .cl */
 	kernel = clCreateKernel(program, "array_add", &ret);
+	printf("kernel creation ret = %d\n", ret);
 
-	int		result[WIDTH * HEIGHT];
-	int 	width = WIDTH;
+
+	int		result[50];
+	int 	max_iter = data->max_iter;
 
 	cl_mem output_buffer;
 
-	output_buffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(int) * WIDTH * HEIGHT, NULL, &ret);
-	clSetKernelArg(kernel, 0, sizeof(int), &data->max_iter);
-	clSetKernelArg(kernel, 1, sizeof(int), &width);
-	clSetKernelArg(kernel, 2, sizeof(cl_mem), &output_buffer);
+	output_buffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(int) * 50, NULL, &ret);
+	printf("buffer ret = %d\n", ret);
+	clSetKernelArg(kernel, 0, sizeof(int), &max_iter);
+	clSetKernelArg(kernel, 1, sizeof(cl_mem), &output_buffer);
 
-	size_t dim = 1;
-	size_t global_size = WIDTH * HEIGHT;
-	clEnqueueNDRangeKernel(queue, kernel, dim, NULL, &global_size, NULL, 0, NULL, NULL);
-	clEnqueueReadBuffer(queue, output_buffer, CL_TRUE, 0, sizeof(int) * WIDTH * HEIGHT, result, 0, NULL, NULL);
+	size_t dim = 2;
+	size_t global_size[] = {5,10};
+	clEnqueueNDRangeKernel(queue, kernel, dim, NULL, global_size, NULL, 0, NULL, NULL);
+	clEnqueueReadBuffer(queue, output_buffer, CL_TRUE, 0, sizeof(int) * 50, result, 0, NULL, NULL);
+
+//	for (int i = 0; i  < 50 ; i++)
+//	{
+//		printf("%d\n", result[i]);
+//	}
+	for (int i = 0; i < 5 ; ++i)
+	{
+		for (int j = 0; j < 10; ++j)
+		{
+			printf("%d\t", result[i * 10 + j]);
+		}
+		printf("\n");
+	}
+
 	clReleaseMemObject(output_buffer);
 	clReleaseKernel(kernel);
 	clReleaseCommandQueue(queue);
 	clReleaseProgram(program);
 	clReleaseContext(context);
 
-	i = 0;
-	int j;
-	while (i < HEIGHT)
-	{
-		j = 0;
-		while (j < WIDTH)
-		{
-//			data->img.img_data[i * WIDTH + j] = result[i * WIDTH + j];
-//			printf("value = %d\n", result[i * WIDTH + j]);
-			j++;
-		}
-		i++;
-	}
-//	printf("value = %d\n", result[2]);
-//	mlx_put_image_to_window(data->mlx, data->win, data->img.img_ptr, 0, 0);
+	mlx_put_image_to_window(data->mlx, data->win, data->img.img_ptr, 0, 0);
 	return (0);
 }
--- krnl.cl
+++ krnl.cl
@@ -3,9 +3,21 @@ static float 	map(float value, float fmin, float fmax, float tmin, float tmax)
 	return ((value - fmin) * (tmax - tmin) / (fmax - fmin) + tmin);
 }
 
-__kernel void array_add(int max_iter, int width, __global int *output)
+__kernel void array_add(int	max_iterations, __global int *output)
 {
-	int x = get_global_id(0);
-
-	output[x] = x;
+	int x = get_global_id(1);
+	int y = get_global_id(0);
+	int width = get_global_size(1);
 }
+
+
+
+/*
+__kernel void array_add(__global int *output)
+{
+	int y = get_global_id(0);
+	int x = get_global_id(1);
+	int size = get_global_size(1);
+	output[y * size + x] = y;
+}
+*/
