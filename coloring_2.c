608b3783d033ba1cd888f80291480a04eb9ecb8e
Dovran Annakuliev <55741979+Dovran-Annakuliev@users.noreply.github.com>
2020-07-26 16:28:35 +0300

    g

---
+++ coloring_2.c
@@ -0,0 +1,44 @@
+#include "../includes/fractol.h"
+
+int				zerg_2(float x, int max)
+{
+	float k;
+	t_rgb color;
+
+	if (x == max)
+		return (BLACK);
+	k = (x / max);
+	color = hsv_to_rgb(250, 0.8, k);
+
+	return (color.c);
+}
+
+int				dark_red(float i, int max)
+{
+	t_rgb color;
+	float k;
+
+	if (i == max)
+		return (BLACK);
+	k = i / (float)max;
+	color.t_rgba.r = (int)(20 * (1 - k) * (k * k * k) * 255);
+	color.t_rgba.g = (int)(1 * ((1 - k) * (1 - k)) * (k * k) * 255);
+	color.t_rgba.b = (int)(1 * ((1 - k) * (1 - k) * (1 - k)) * k * 255);
+	color.t_rgba.a = 0;
+	return (color.c);
+}
+
+int				mix(float i, int max)
+{
+	t_rgb color;
+	float k;
+
+	if (i == max)
+		return (BLACK);
+	k = i / (float)max;
+	color.t_rgba.r = (int)(1 * ((1 - k) * (1 - k) * (1 - k)) * k * 255);
+	color.t_rgba.g = (int)(15 * ((1 - k) * (1 - k)) * (k * k) * 255);
+	color.t_rgba.b = (int)(20 * (1 - k) * (k * k * k) * 255);
+	color.t_rgba.a = 0;
+	return (color.c);
+}
